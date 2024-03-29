6242c8686a3ee09c899ecf8b91c7531b50a04172
Awalu-Levison <levisonawalu251@gmail.com>
2024-03-29 08:32:43 -0700

    A function that prints a character at a time

---
+++ char_print.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * char_print - Printing a character at a time
+ * @z: The character passed to the function
+ * Return: Number of characters printed
+ */
+int char_print(char z)
+{
+	return write(1, &z, 1);
+}
