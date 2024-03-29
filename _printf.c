bbad94d262470dfc77673ed91a75167334b0f804
Awalu-Levison <levisonawalu251@gmail.com>
2024-03-29 08:59:52 -0700

    The customized function of standard printf function

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _printf - Customize the standard printf function
+ * @format: The format specifier
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, j = 0;
+
+	va_list args;
+
+	va_start(args, format);
+
+	for (; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			char_print(format[i]);
+		}
+		else if (format[i + 1] == 'c')
+		{
+			char_print(va_arg(args, int));
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			char *mystr = va_arg(args, char *);
+
+			int x = str_print(mystr);
+
+			j += (x - 1);
+			i++;
+		}
+		else if (format[i + 1] == '%')
+		{
+			char_print('%');
+			i++;
+		}
+		j += 1;
+
+	}
+	return (j);
+}
