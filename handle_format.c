8d833c8fd82537e6cf3fb976f59cc6ff57422c74
Awalu-Levison <levisonawalu251@gmail.com>
2024-03-31 01:20:23 -0700

    A function that handles the format given by user

---
+++ handle_format.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * handle_format - Dealing with the specifier
+ * given by the user
+ * @format: Specifier given by user
+ * @mylist: Variable number of arguments
+ * passed to the function
+ * Return: Number of characters printed
+ */
+int handle_format(const char *format, va_list mylist)
+{
+	unsigned int i = 0;
+
+	if (*format == 'c')
+	{
+		i += char_print(va_arg(mylist, int));
+	}
+	else if (*format == 's')
+	{
+		i += str_print(va_arg(mylist, char *));
+	}
+	else if (*format == '%')
+	{
+		i += char_print('%');
+	}
+	return (i);
+}
