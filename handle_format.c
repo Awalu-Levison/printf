9949e7f3c483304248cc51e2f2ac5f90cb752907
Awalu-Levison <levisonawalu251@gmail.com>
2024-03-31 19:56:39 -0700

    Handle format specifiers

--- handle_format.c
+++ handle_format.c
@@ -7,21 +7,23 @@
  * passed to the function
  * Return: Number of characters printed
  */
-int handle_format(const char *format, va_list mylist)
+int handle_format(char format, va_list mylist)
 {
 	unsigned int i = 0;
 
-	if (*format == 'c')
-	{
+	if (format == 'c')
 		i += char_print(va_arg(mylist, int));
-	}
-	else if (*format == 's')
-	{
+
+	else if (format == 's')
 		i += str_print(va_arg(mylist, char *));
-	}
-	else if (*format == '%')
-	{
-		i += char_print('%');
-	}
+
+	else if (format == 'd')
+		i += digit_print((long)va_arg(mylist, int), 10);
+
+	else if (format == 'x')
+		i += digit_print((long)va_arg(mylist, unsigned int), 16);
+
+	else
+		i += write(1, &format, 1);
 	return (i);
 }
