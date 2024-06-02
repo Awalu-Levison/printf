eb68878d34dcd295313d0af6778313802b888b73
Awalu-Levison <levisonawalu251@gmail.com>
2024-06-02 04:01:56 -0700

    Deleting some files

--- main.h
+++ main.h
@@ -6,15 +6,5 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/*User defined prototypes*/
-int handle_format(char format, va_list mylist);
-int char_print(int z);
-int length_str(char *mystr);
-int get_int(int num);
-int _printf(const char *format, ...);
-int digit_print(long num, int base);
-int string_print(char *mystr);
-int _strlen(char *mystr);
-
 
 #endif
---
+++ putchr.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _putchr - A function thst print a character
+ * @Return: Number of characters printed
+ */
+int _putchr(char c)
+{
+	return (write(1, &c, 1);
+}
