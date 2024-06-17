94b3015c6ef83106607746a440446ac38f305460
Awalu-Levison <levisonawalu251@gmail.com>
2024-06-17 04:37:38 -0700

    Organise files

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _printf - A custom version of printf function
+ * @format: The format specifier
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+}
--- putchr.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * _putchr - A function thst print a character
- * @Return: Number of characters printed
- */
-int _putchr(char c)
-{
-	return (write(1, &c, 1);
-}
